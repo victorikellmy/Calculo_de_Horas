872203b52fdd1a7c9ff0f8b471c962be73aecb34
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-03-16 21:16:10 -0300

    Arrumando App.js

--- app/distancia/src/App.js
+++ app/distancia/src/App.js
@@ -2,42 +2,45 @@ import React, { useState } from 'react';
 
 
 function App() {
-    const[distancia, setDis] = useState(0);
-    const[velociodade,setVel] = useState(0);
-    const[calculo, setCal] = useState(0);
+  const [distancia, setDis] = useState(0);
+  const [velociodade, setVel] = useState(0);
+  const [calculo, setCal] = useState(0);
 
-    function calcular(e){
-      e.preventDefault();
-      setCal(distancia/velociodade);
-    }
+  function calcular(e) {
+    e.preventDefault();
+    const tempoEmHoras = distancia/velociodade;
+    const horas = Math.floor(tempoEmHoras);
+    const minutos = Math.round((tempoEmHoras - horas) * 60);
+    setCal(`${horas}h ${minutos}min`);
+  }
 
-    return (
-    <div onSubmit={calcular}>
+  return (
+    <div >
       <h1>Cadastrando</h1>
-      <form>
-        <label>Distancia</label><br/>
+      <form onSubmit={calcular}>
+        <label>Distancia</label><br />
         <input placeholder="Digite a distancia em km"
-        value={distancia}
-        onChange={(e) => setDis(e.target.value) }
-        /><br/>
+          value={distancia}
+          onChange={(e) => setDis(e.target.value)}
+        /><br />
 
-        <label>Velocidade</label><br/>
+        <label>Velocidade</label><br />
         <input placeholder="Velociadade em km/h"
-        value={velociodade}
-        onChange={(e)=> setVel(e.target.value) }
-      /><br/>
+          value={velociodade}
+          onChange={(e) => setVel(e.target.value)}
+        /><br />
 
-      <button type='submit'>Calcular</button>
+        <button type='submit'>Calcular</button>
       </form>
 
       <div>
-        <span>Distancia:{contador}</span><br/>
+        <span>Distancia:{calculo}</span><br />
       </div>
 
     </div>
 
   );
-  
+
 }
 
 export default App;
